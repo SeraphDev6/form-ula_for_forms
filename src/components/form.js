692abe1463c6875d361976398c58223cc3802249
SeraphDev6 <wesley.giles@gmail.com>
2021-11-04 20:12:09 -0400

    Added validation and conditional rendering

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,12 @@
 import react,{useState} from "react";
 import styles from './form.module.css'
-
+const re= /\S+@\S+\.\S+/;
 const InputBox = (props)=>{
     const {name,type,event} =props;
     const id=name.toLowerCase() .split(" ").join("_");
     return(
         <div className={styles.inputBox}>
-            <label for={id}>
+            <label htmlFor={id}>
                 {name}  <input name={id} id={id} type={type} onChange={event}/>
             </label>
         </div>
@@ -14,10 +14,15 @@ const InputBox = (props)=>{
 }
 const Form = (props)=>{
     const [firstname, setFirstname] = useState("");
+    const [firstnameError, setFirstnameError] = useState("");
     const [lastname, setLastname] = useState("");
+    const [lastnameError, setLastnameError] = useState("");
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
     const [confirm, setConfirm] = useState("");
+    const [confirmError, setConfirmError] = useState("");
 
     const createUser = (e) =>{
         e.preventDefault();
@@ -25,32 +30,80 @@ const Form = (props)=>{
         console.log("Welcome",newUser)
     }
 
+    const handleFirstname= (e)=>{
+        setFirstname(e.target.value);
+        if(e.target.value.length<1){
+            setFirstnameError("First Name is required");
+        }else if(e.target.value.length<3){
+            setFirstnameError("First Name must be 3 or more characters");
+        }else if(e.target.value.length>44){
+            setFirstnameError("First Name must be less than 45 characters");
+        }else{
+            setFirstnameError("");
+        }
+    }
+    const handleLastname= (e)=>{
+        setLastname(e.target.value);
+        if(e.target.value.length<1){
+            setLastnameError("Last Name is required");
+        }else if(e.target.value.length<3){
+            setLastnameError("Last Name must be 3 or more characters");
+        }else if(e.target.value.length>44){
+            setLastnameError("Last Name must be less than 45 characters");
+        }else{
+            setLastnameError("");
+        }
+    }
+    const handleEmail= (e)=>{
+        setEmail(e.target.value);
+        if(e.target.value.length<1){
+            setEmailError("Email is required");
+        }else if(e.target.value.length>=60){
+            setEmailError("Email must be less than 60 characters");
+        }else if(!re.test(e.target.value)){
+            setEmailError("Not a valid Email");
+        }else{
+            setEmailError("");
+        }
+    }
+    const handlePassword= (e)=>{
+        setPassword(e.target.value);
+        if(e.target.value.length<1){
+            setPasswordError("Password is required");
+        }else if(e.target.value.length<8){
+            setPasswordError("Password must be 8 or more characters");
+        }else if(e.target.value.length>=60){
+            setPasswordError("Password must be less than 60 characters");
+        }else{
+            setPasswordError("");
+        }
+    }
+    const handleConfirm= (e)=>{
+        setConfirm(e.target.value);
+        if(e.target.value!==password){
+            setConfirmError("Password's must match!");
+        }else{
+            setConfirmError("");
+        }
+    }
     return(
         <div id={styles.container}>
             <form onSubmit={createUser}>
-                <InputBox name="First Name" type="text" event={(e)=>setFirstname(e.target.value)}/>
-                <InputBox name="Last Name" type="text" event={(e)=>setLastname(e.target.value)}/>
-                <InputBox name="Email" type="email" event={(e)=>setEmail(e.target.value)}/>
-                <InputBox name="Password" type="password" event={(e)=>setPassword(e.target.value)}/>
-                <InputBox name="Confirm Password" type="password" event={(e)=>setConfirm(e.target.value)}/>
+                <InputBox name="First Name" type="text" event={handleFirstname}/>
+                {firstnameError?<p className={styles.warning}>{firstnameError}</p>:""}
+                <InputBox name="Last Name" type="text" event={handleLastname}/>
+                {lastnameError?<p className={styles.warning}>{lastnameError}</p>:""}
+                <InputBox name="Email" type="email" event={handleEmail}/>
+                {emailError?<p className={styles.warning}>{emailError}</p>:""}
+                <InputBox name="Password" type="password" event={handlePassword}/>
+                {passwordError?<p className={styles.warning}>{passwordError}</p>:""}
+                <InputBox name="Confirm Password" type="password" event={handleConfirm}/>
+                {confirmError?<p className={styles.warning}>{confirmError}</p>:""}
+                {(firstnameError.length+lastnameError.length+emailError.length+passwordError.length+confirmError.length)<1?
+                <input type='submit' value="submit" />:
+                <input type='submit' value="submit" disabled/>
+                }
             </form>
-            <h3>Your Form Data</h3>
-            <div className={styles.results}>
-            <div className={styles.labels}>
-                <p>First Name</p>
-                <p>Last Name</p>
-                <p>Email</p>
-                <p>Password</p>
-                <p>Confirm Password</p>
-            </div>
-            <div className={styles.data}>
-                <p>{firstname}</p>
-                <p>{lastname}</p>
-                <p>{email}</p>
-                <p>{password}</p>
-                <p>{confirm}</p>
-            </div>
-            </div>
         </div>
     )
 }
